a3aa69fa143181ce51c31dbcd840148103145bd6
Bruno Guimarães <brunoguimaraes.dev@gmail.com>
2025-04-07 18:40:13 -0300

    fix(ui-test): Library issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.bgbrlk.scoreboardbrlk"
         minSdk = 26
         targetSdk = 35
-        versionName = "1.3.12"
+        versionName = "1.3.13"
         versionCode = getVersionCode()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         archivesName = getCustomVersionName()
