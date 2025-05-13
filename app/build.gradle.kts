db545f063ff1b650df08915142ad87bc3ae8a046
Bruno Guimarães <brunoguimaraes.dev@gmail.com>
2025-05-12 23:24:53 -0300

    Add(.gitignore): kls_database.db, Incremented version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.bgbrlk.scoreboardbrlk"
         minSdk = 26
         targetSdk = 35
-        versionName = "1.3.14"
+        versionName = "1.3.15"
         versionCode = getVersionCode()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         archivesName = getCustomVersionName()
